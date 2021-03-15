2c697d2c1dd73faf905dee397e8b97c1e492b233
Jose Caceres <josecaceres.oreul@gmail.com>
2021-03-15 12:30:47 -0500

    Modify test

--- __tests__/src/v1/scenes/logged/customers/create/components/form.test.js
+++ __tests__/src/v1/scenes/logged/customers/create/components/form.test.js
@@ -98,7 +98,7 @@ describe('Form component', () => {
     test('onPress functionality', () => {
         const onPressEvent = jest.fn();
         onPressEvent.mockReturnValue('Link on press invoked');
-        const wrapper = shallow(<Form createCustomer={onPressEvent} isValidForm />);
+        const wrapper = shallow(<Form nextAddressSelect={onPressEvent} isValidForm />);
 
         wrapper.find(Button).first().props().onPress();
         expect(onPressEvent.mock.results[0].value).toBe('Link on press invoked');
@@ -108,7 +108,7 @@ describe('Form component', () => {
         let isPress = true;
         const onPressEvent = jest.fn();
         onPressEvent.mockReturnValue('Link on press invoked');
-        const wrapper = shallow(<Form createCustomer={onPressEvent} isValidForm={false} />);
+        const wrapper = shallow(<Form nextAddressSelect={onPressEvent} isValidForm={false} />);
 
         try {
             wrapper.find(TouchableOpacity).first().props().onPress();
