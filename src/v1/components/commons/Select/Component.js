76ddeaba1e5c2dadd08aafeb9ce46d7c61577c32
Jose Caceres <josecaceres.oreul@gmail.com>
2021-03-15 11:18:01 -0500

    Add Component in Select

---
+++ src/v1/components/commons/Select/Component.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {TouchableOpacity, Modal, View, ScrollView} from 'react-native';
+
+import TextField from '../../../components/commons/TextField';
+import Button from '../../../components/commons/Button';
+
+import styles from './styles';
+
+const Component = (props) => (
+    <>
+        <Modal
+            animationType="none"
+            transparent={true}
+            visible={props.isVisibleSelect}
+            onRequestClose={props.hiddenSelect}>
+            <View style={styles.container}>
+                <View style={styles.content}>
+                    <ScrollView>
+                        {props.options.map((option) => (
+                            <TouchableOpacity
+                                key={`Select${option.label}`}
+                                style={styles.item_content}
+                                onPress={() => {
+                                    props.onSelect(option);
+                                    props.hiddenSelect();
+                                }}>
+                                <TextField text={option.label} />
+                            </TouchableOpacity>
+                        ))}
+                    </ScrollView>
+                </View>
+                <Button
+                    isGreenButton
+                    text={'Salir'}
+                    onPress={props.hiddenSelect}
+                />
+            </View>
+        </Modal>
+        <TouchableOpacity onPress={props.options.length > 0 ? props.showSelect : null} style={styles.button_content}>
+            <TextField text={props.value ? props.value : 'Seleccionar'} />
+        </TouchableOpacity>
+    </>
+);
+
+Component.propTypes = {
+    /**
+     * Valor del select.
+     */
+    value: PropTypes.string,
+    /**
+     * Opciones del select.
+     */
+    options: PropTypes.array,
+    /**
+     * Funcion que devuelve el dato seleccionado.
+     */
+    onSelect: PropTypes.func,
+    /**
+     * Funcion que muestra el selector (Modal).
+     */
+    showSelect: PropTypes.func,
+    /**
+     * Funcion que oculta el selector (Modal).
+     */
+    hiddenSelect: PropTypes.func,
+    /**
+     * Determina si el selector se encuentra visible.
+     */
+    isVisibleSelect: PropTypes.bool,
+};
+
+Component.defaultProps = {
+    value: '',
+    options: [],
+    onSelect: () => {},
+    showSelect: () => {},
+    hiddenSelect: () => {},
+    isVisibleSelect: false,
+};
+
+export default Component;
--- src/v1/components/commons/Select/index.js
+++ src/v1/components/commons/Select/index.js
@@ -1,55 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {TouchableOpacity, Modal, View, ScrollView} from 'react-native';
 
-import TextField from '../../../components/commons/TextField';
-import Button from '../../../components/commons/Button';
+import Component from './Component';
 import useSelect from '../../../hooks/useSelect';
-
-import styles from './styles';
 
 /**
  * Este componente renderiza un select basico.
  */
 const Select = (props) => {
-    const {onSelect, value, options} = props;
-    const {isVisibleSelect, showSelect, hiddenSelect} = useSelect();
-
-    return (
-        <>
-            <Modal
-                animationType="none"
-                transparent={true}
-                visible={isVisibleSelect}
-                onRequestClose={hiddenSelect}>
-                <View style={styles.container}>
-                    <View style={styles.content}>
-                        <ScrollView>
-                            {options.map((option) => (
-                                <TouchableOpacity
-                                    key={`Select${option.label}`}
-                                    style={styles.item_content}
-                                    onPress={() => {
-                                        onSelect(option);
-                                        hiddenSelect();
-                                    }}>
-                                    <TextField text={option.label} />
-                                </TouchableOpacity>
-                            ))}
-                        </ScrollView>
-                    </View>
-                    <Button
-                        isGreenButton
-                        text={'Salir'}
-                        onPress={hiddenSelect}
-                    />
-                </View>
-            </Modal>
-            <TouchableOpacity onPress={options.length > 0 ? showSelect : null} style={styles.button_content}>
-                <TextField text={value ? value : 'Seleccionar'} />
-            </TouchableOpacity>
-        </>
-    );
+    const hook = useSelect();
+    return <Component {...props} {...hook} />;
 };
 
 Select.propTypes = {
